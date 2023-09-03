7b470430c7f0f22f7dd8cda846923248611e200e
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-09-03 11:16:22 +0600

    Routes dir setup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div className='text-pink-700'>LoginPage</div>
+}
+
+export default LoginPage
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const RootPage = () => {
+  return <div className='text-red-600 text-4xl'>Goodbye RootPage</div>
+}
+
+export default RootPage

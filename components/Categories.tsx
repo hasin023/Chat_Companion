887b16bec7ef56fa2a44d55a631661b85b57c49a
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-09-04 17:14:59 +0600

    Added category filter buttons

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import qs from "query-string"
 import { Category } from "@prisma/client"
-import React from "react"
+import { useRouter, useSearchParams } from "next/navigation"
 
 import { cn } from "@/lib/utils"
 
@@ -10,23 +11,74 @@ interface CategoriesProps {
 }
 
 const Categories = ({ data }: CategoriesProps) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const categoryId = searchParams.get("categoryId")
+
+  const onClick = (id: string | undefined) => {
+    const query = { categoryId: id }
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    )
+
+    router.push(url)
+  }
+
   return (
     <div className='w-full overflow-x-auto space-x-2 flex p-1'>
       <button
+        onClick={() => onClick(undefined)}
         className={cn(
-          "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition"
+          `
+          flex 
+          items-center 
+          text-center 
+          text-xs 
+          md:text-sm 
+          px-2 
+          md:px-4 
+          py-2 
+          md:py-3 
+          rounded-md 
+          bg-primary/10 
+          hover:opacity-75 
+          transition
+        `,
+          !categoryId ? "bg-primary/25" : "bg-primary/10"
         )}
       >
         Newest
       </button>
-      {data.map((category) => (
+      {data.map((item) => (
         <button
-          key={category.id}
+          onClick={() => onClick(item.id)}
           className={cn(
-            "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition"
+            `
+            flex 
+            items-center 
+            text-center 
+            text-xs 
+            md:text-sm 
+            px-2 
+            md:px-4 
+            py-2 
+            md:py-3 
+            rounded-md 
+            bg-primary/10 
+            hover:opacity-75 
+            transition
+          `,
+            item.id === categoryId ? "bg-primary/25" : "bg-primary/10"
           )}
+          key={item.id}
         >
-          {category.name}
+          {item.name}
         </button>
       ))}
     </div>
